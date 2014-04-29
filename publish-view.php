48efa71af1dc8b23229fd31fb3cf2eeee633c79f
launchinteractive <launchinteractive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-29 02:16:37 +0000

    Initial Import
    git-svn-id: https://plugins.svn.wordpress.org/publish-view/trunk@904516 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ publish-view.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Name: Publish & View
+ * Plugin URI: http://launchinteractive.com.au/wordpress/publish-view.zip
+ * Description: Adds a button so you can Publish and View Pages, Posts etc. in one step.
+ * Version: 1.3
+ * Author: Marc Castles
+ * Author URI: http://launchinteractive.com.au
+ * License: GPL2
+*/
+/*
+Copyright 2014  Marc Castles  (email : marc@launchinteractive.com.au)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function publish_view_enqueue() {
+	global $post;
+	if(isset($post) && $post->post_type != 'acf' && in_array($post->post_status,array('auto-draft','draft','publish'))) {
+    	global $wp_version;
+    	if($wp_version < 3.8) {
+	    	wp_register_style( 'publish-view-dashicons', plugins_url('publish-view-dashicons.css', __FILE__) );
+	    	wp_enqueue_style( 'publish-view-dashicons' );
+    	}
+		
+		wp_register_style( 'publish-view', plugins_url('publish-view.css', __FILE__) );
+    	wp_enqueue_style( 'publish-view' );
+
+	}
+}
+add_action( 'admin_enqueue_scripts', 'publish_view_enqueue' );
+	 
+function publish_view_submitbox_start(){
+	global $post;
+	global $wp_version;
+	
+	if($post->post_type != 'acf') {
+		
+		if($post->post_status == 'auto-draft' || $post->post_status == 'draft') {
+			submit_button('&#xf177;','primary','publish',false, array('onclick'=>"jQuery(this).after('<input type=\"hidden\" name=\"publishview\" value=\"Y\" />')",'title'=>'Publish & View','id'=>'publishview'));
+		} else if($post->post_status == 'publish') {
+			submit_button('&#xf177;','primary','publish',false, array('onclick'=>"jQuery(this).after('<input type=\"hidden\" name=\"publishview\" value=\"Y\" />')",'title'=>'Update & View','id'=>'publishview'));
+		}
+	}
+}
+add_action( 'post_submitbox_start', 'publish_view_submitbox_start' );
+
+function publish_view_redirect($location)
+{
+    global $post;
+    if (isset($_POST['publishview'])) {
+			$location = get_permalink($post->ID);
+    }
+    return $location;
+}
+add_filter('redirect_post_location', 'publish_view_redirect');
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Publish View ===
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VL5VBHN57FVS2
+Contributors:launchinteractive
+Tags: save, view, publish, update
+Requires at least: 3.6.1
+Stable tag: trunk
+Tested up to: 3.9
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a button so you can Publish and View Pages, Posts etc. in one step.
+
+== Installation ==
+
+1. Upload the `publish-view` folder to the `/wp-content/plugins/` directory or upload the zip within WordPress
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 1.3 =
+* Prefix functions to avoid conflicts
+
+= 1.2 =
+* Hide button for ACF
+* Fix position for Woocommerce
+* Include fonts for old WP rather than linking to FontAwesome
+
+= 1.1 =
+* Added Support for WP 3.6+
+
+= 1.0 =
+* Initial Release.
