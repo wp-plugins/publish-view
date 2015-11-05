fa926f6583b835be1bf65f77be73aab7623dc98b
launchinteractive <launchinteractive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-05 04:28:01 +0000

    * Add option per user to open new window for Publish View button
    git-svn-id: https://plugins.svn.wordpress.org/publish-view/trunk@1279991 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- publish-view.php
+++ publish-view.php
@@ -3,7 +3,7 @@
  * Plugin Name: Publish & View
  * Plugin URI: https://wordpress.org/plugins/publish-view
  * Description: Adds a button so you can Publish and View Pages, Posts etc. in one step.
- * Version: 2
+ * Version: 2.1
  * Author: Launch Interactive
  * Author URI: http://launchinteractive.com.au
  * License: GPL2
@@ -44,10 +44,12 @@ class PublishView {
 	}
 	
 	public function extra_profile_fields( $user ) {
-	    $newwindow = get_user_meta( $user->ID, 'pv_newwindow', true );
+	    $previewNewWindow = get_user_meta( $user->ID, 'pv_new_window', true );
+	    $publishNewWindow = get_user_meta( $user->ID, 'pv_publish_new_window', true );
 	    ?>
 	    <h3>Publish View Options</h3>
-	    <table class="form-table"><tr><th>New Window</th><td><label><input type="checkbox" value="checked" name="pv_newwindow" <?php echo ( $newwindow=='' ? 'checked ' :'' ); ?>/> Open new window for <?php echo __('Preview Changes'); ?> or <?php echo __('View Page'); ?> links.</label></td></tr></table>
+	    <table class="form-table"><tr><th>Preview New Window</th><td><label><input type="checkbox" value="checked" name="pv_new_window" <?php echo ( $previewNewWindow=='' ? 'checked ' :'' ); ?>/> Open new window for <?php echo __('Preview Changes'); ?> or <?php echo __('View Page'); ?> links.</label></td></tr>
+	    <tr><th>Publish New Window</th><td><label><input type="checkbox" value="checked" name="pv_publish_new_window" <?php echo ( $publishNewWindow=='enabled' ? 'checked ' : '' ); ?>/> Open new window for Publish View Button.</label></td></tr></table>
 	    <?php
 	}
 	
@@ -55,11 +57,17 @@ class PublishView {
 
 		if ( !current_user_can( 'edit_user', $user_id ) )
 			return false;
-			
-		if(isset($_POST['pv_newwindow'])) {
-			delete_usermeta( $user_id, 'pv_newwindow');
+		
+		if(isset($_POST['pv_publish_new_window'])) {
+			update_usermeta( $user_id, 'pv_publish_new_window', 'enabled' );
 		} else {
-			update_usermeta( $user_id, 'pv_newwindow', 'disable' );
+			delete_usermeta( $user_id, 'pv_publish_new_window');
+		}
+		
+		if(isset($_POST['pv_new_window'])) {
+			delete_usermeta( $user_id, 'pv_new_window');
+		} else {
+			update_usermeta( $user_id, 'pv_new_window', 'disable' );
 		}
 	}
 	
@@ -86,15 +94,30 @@ class PublishView {
 		$type = get_post_type_object( $post->post_type );
 		if($type->public) {
 			
+			$options = array();
+			
 			if($post->post_status == 'auto-draft' || $post->post_status == 'draft') {
-				submit_button('&#xf177;','primary','publish',false, array('onclick'=>"jQuery(this).after('<input type=\"hidden\" name=\"publishview\" value=\"Y\" />')",'title'=>'Publish & View','id'=>'publishview'));
+				$options = array('onclick'=>"jQuery(this).after('<input type=\"hidden\" name=\"publishview\" value=\"Y\" />')",'title'=>'Publish & View','id'=>'publishview');
 			} else if($post->post_status == 'publish') {
-				submit_button('&#xf177;','primary','publish',false, array('onclick'=>"jQuery(this).after('<input type=\"hidden\" name=\"publishview\" value=\"Y\" />')",'title'=>'Update & View','id'=>'publishview'));
+				$options = array('onclick'=>"jQuery(this).after('<input type=\"hidden\" name=\"publishview\" value=\"Y\" />')",'title'=>'Update & View','id'=>'publishview');
+			}
+			
+			if(count($options) > 0) {
+			
+				$publishNewWindow = get_user_meta( get_current_user_id(), 'pv_publish_new_window', true );
+			
+				if($publishNewWindow == 'enabled') {
+
+					$options['onclick'] .= ";jQuery(this).closest('form').attr('target','_blank');setTimeout(function(){jQuery('#submitpost .disabled').removeClass('disabled');jQuery('#submitpost .spinner').removeClass('is-active');wp.autosave.server.resume()},1000);";
+
+				}
+			
+				submit_button('&#xf177;','primary','publish',false, $options);
 			}
 		}
 		
-		$newwindow = get_user_meta( get_current_user_id(), 'pv_newwindow', true );
-		if($newwindow == 'disable') {
+		$previewNewWindow = get_user_meta( get_current_user_id(), 'pv_new_window', true );
+		if($previewNewWindow == 'disable') {
 			//remove preview new tab/page
 			echo '<script>jQuery(\'#post-preview\').attr(\'target\',\'_self\');jQuery(\'#message.updated a,#view-post-btn a\').removeAttr(\'target\');</script>';
 		}
@@ -103,6 +126,8 @@ class PublishView {
 	public function redirect($location) {
     global $post;
     if (isset($_POST['publishview'])) {
+
+
 			$location = get_permalink($post->ID);
     }
     return $location;
--- readme.txt
+++ readme.txt
@@ -16,6 +16,9 @@ Adds a button so you can Publish and View Pages, Posts etc. in one step.
 2. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Changelog ==
+= 2.1 =
+* Add option per user to open new window for Publish View button
+
 = 2 =
 * Reformat plugin code
 * Make New Window optional per user and default to checked
